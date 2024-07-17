e614604a0bfe77739c63691b06431b7e3be75694
AlexF43 <97276854+AlexF43@users.noreply.github.com>
2024-07-17 17:22:37 +1000

    removed legacy db context code from migration to postgresql from sqlserver

--- AuxAPI/Data/ApplicationDbContext.cs
+++ AuxAPI/Data/ApplicationDbContext.cs
@@ -9,22 +9,5 @@ public class ApplicationDbContext : DbContext
     {
     }
     
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-
-        // Configure all strings to use 'text' by default
-        foreach (var entity in modelBuilder.Model.GetEntityTypes())
-        {
-            foreach (var property in entity.GetProperties())
-            {
-                if (property.ClrType == typeof(string))
-                {
-                    property.SetColumnType("text");
-                }
-            }
-        }
-    }
-
     public DbSet<TestItem> TestItems { get; set; }
 }
